92dfc387026170186dafeaa3ccc16bb0874624b3
sokoloff (Alexander) <sokoloff.a@gmail.com>
2014-03-24 20:37:39 +0400

    Translations

---
+++ gui/translations/boomaga_de.ts
@@ -0,0 +1,349 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.0">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source> About Boomaga</source>
+        <translation> Über Boomaga</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation>Autoren</translation>
+    </message>
+    <message>
+        <source>Thanks</source>
+        <translation>Vielen Dank</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Übersetzungen</translation>
+    </message>
+    <message>
+        <source>Version: %1</source>
+        <translation>Version: %1</translation>
+    </message>
+    <message>
+        <source>Boomaga provides a virtual printer for CUPS. This can be used for print preview or for print booklets.</source>
+        <translation>Boomaga stellt einen virtuellen Drucker für CUPS zur Verfügung. Dieser kann als Druckvorschau oder zum Drucken von Booklets verwendet werden.</translation>
+    </message>
+    <message>
+        <source>Copyright: %1-%2 %3</source>
+        <translation>Copyright: %1-%2 %3</translation>
+    </message>
+    <message>
+        <source>Homepage: %1</source>
+        <translation>Homepage: %1</translation>
+    </message>
+    <message>
+        <source>License: %1</source>
+        <translation>Lizenz: %1</translation>
+    </message>
+    <message>
+        <source>Boomaga is developed by the &lt;a %1&gt;Boomaga Team and contributors&lt;/a&gt; on Github.</source>
+        <translation>Boomaga wird vom &lt;a %1&gt;Boomaga Team und Freiwilligen&lt;/a&gt; auf Github entwickelt.</translation>
+    </message>
+    <message>
+        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
+        <translation>Wer mit dem Entwickler-Team zusammenarbeiten möchte, bitte &lt;a %1&gt;hier melden&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>Special thanks to:</source>
+        <translation>Besonderen Dank an:</translation>
+    </message>
+    <message>
+        <source>If you want to help translate, we will be glad to see you in our translation team on &lt;a %1&gt;Transifex server&lt;/a&gt;.</source>
+        <translation>Wenn Du zur Übersetzung beitragen möchtest, würden wir uns freuen, dich im Übersetzungsteam auf &lt;a %1&gt;Transifex&lt;/a&gt; begrüßen zu dürfen.</translation>
+    </message>
+</context>
+<context>
+    <name>Job</name>
+    <message>
+        <source>Untitled</source>
+        <translation>Unbenannt</translation>
+    </message>
+</context>
+<context>
+    <name>JobsListView</name>
+    <message>
+        <source>Rename job</source>
+        <translation>Druckauftrag umbenennen</translation>
+    </message>
+    <message>
+        <source>Delete job</source>
+        <translation>Druckauftrag löschen</translation>
+    </message>
+    <message>
+        <source>Job title:</source>
+        <translation>Titel des Druckauftrags:</translation>
+    </message>
+    <message>
+        <source>( %1 pages )</source>
+        <translation>( %1 Seiten )</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Einstellungen</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Optionen</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>Gestaltung</translation>
+    </message>
+    <message>
+        <source>1 up</source>
+        <translation>1 Seite</translation>
+    </message>
+    <message>
+        <source>2 up</source>
+        <translation>2 Seiten</translation>
+    </message>
+    <message>
+        <source>4 up</source>
+        <translation>4 Seiten</translation>
+    </message>
+    <message>
+        <source>8 up</source>
+        <translation>8 Seiten</translation>
+    </message>
+    <message>
+        <source>Booklet</source>
+        <translation>Broschüre</translation>
+    </message>
+    <message>
+        <source>Print on both sides of a sheet.</source>
+        <comment>MainForm::Double-sided checkbox tooltip</comment>
+        <translation>Doppelseitiger Druck.</translation>
+    </message>
+    <message>
+        <source>Double-sided</source>
+        <comment>MainForm::Double-sided checkbox</comment>
+        <translation>Doppelseitig</translation>
+    </message>
+    <message>
+        <source>Jobs</source>
+        <translation>Druckaufträge</translation>
+    </message>
+    <message>
+        <source>Printer</source>
+        <translation>Drucker</translation>
+    </message>
+    <message>
+        <source>Configure ...</source>
+        <translation>Konfigurieren ...</translation>
+    </message>
+    <message>
+        <source>Toolbar</source>
+        <translation>Werkzeugleiste</translation>
+    </message>
+    <message>
+        <source>&amp;Print</source>
+        <translation>&amp;Drucken</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <source>Print and Close</source>
+        <translation>Drucken und Beenden</translation>
+    </message>
+    <message>
+        <source>Ctrl+P</source>
+        <translation>Strg+P</translation>
+    </message>
+    <message>
+        <source>Next sheet</source>
+        <translation>Nächstes Blatt</translation>
+    </message>
+    <message>
+        <source>Previous sheet</source>
+        <translation>Vorheriges Blatt</translation>
+    </message>
+    <message>
+        <source>About Boomaga</source>
+        <translation>Über Boomaga</translation>
+    </message>
+    <message>
+        <source>Boomaga</source>
+        <translation>Boomaga</translation>
+    </message>
+    <message>
+        <source>%v of %m</source>
+        <comment>Format for QProgressBar (http://qt-project.org/doc/qt-4.8/qprogressbar.html#format-prop)</comment>
+        <translation>%v von %m</translation>
+    </message>
+    <message>
+        <source>Print the all pages on %1.</source>
+        <translation>Alle Seiten auf %1 drucken.</translation>
+    </message>
+    <message>
+        <source>Print the odd pages on %1.&lt;p&gt;When finished, turn the pages, insert them into the printer&lt;br&gt;and click the Continue button.</source>
+        <translation>Ungerade Seitenzahlen auf %1 drucken.&lt;p&gt;Wenn der Ausdruck beendet ist, Seiten umdrehen, wieder in den Drucker einlegen&lt;br&gt;und die Schaltfläche Weiter betätigen.</translation>
+    </message>
+    <message>
+        <source>Print the even pages on %1.</source>
+        <translation>Gerade Seitenzahlen auf %1 drucken.</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Weiter</translation>
+    </message>
+    <message>
+        <source>%1 pages</source>
+        <comment>Status bar</comment>
+        <translation>%1 Seiten</translation>
+    </message>
+    <message>
+        <source>%1 page</source>
+        <comment>Status bar</comment>
+        <translation>%1 Seite</translation>
+    </message>
+    <message>
+        <source>%1 sheets</source>
+        <comment>Status bar</comment>
+        <translation>%1 Blätter</translation>
+    </message>
+    <message>
+        <source>%1 sheet</source>
+        <comment>Status bar</comment>
+        <translation>%1 Blatt</translation>
+    </message>
+    <message>
+        <source>Sheet %1 of %2</source>
+        <comment>Status bar</comment>
+        <translation>Blatt %1 von %2</translation>
+    </message>
+    <message>
+        <source>Insert blank page after this page</source>
+        <translation>Leere Seite nach dieser Seite einfügen</translation>
+    </message>
+    <message>
+        <source>Delete this page</source>
+        <translation>Diese Seite löschen</translation>
+    </message>
+    <message>
+        <source>Undo delete</source>
+        <translation>Rückgängig löschen</translation>
+    </message>
+    <message>
+        <source>Insert blank page before this page</source>
+        <translation>Leere Seite vor dieser Seite einfügen</translation>
+    </message>
+    <message>
+        <source>Delete pages until the end of the job.</source>
+        <translation>Seiten ab hier bis zum Ende löschen.</translation>
+    </message>
+</context>
+<context>
+    <name>PrinterSettings</name>
+    <message>
+        <source>Margins (mm)</source>
+        <translation>Ränder (mm)</translation>
+    </message>
+    <message>
+        <source>Right:</source>
+        <translation>Rechts:</translation>
+    </message>
+    <message>
+        <source>Bottom:</source>
+        <translation>Unten:</translation>
+    </message>
+    <message>
+        <source>Internal:</source>
+        <translation>Intern:</translation>
+    </message>
+    <message>
+        <source>Left:</source>
+        <translation>Links:</translation>
+    </message>
+    <message>
+        <source>Top:</source>
+        <translation>Oben:</translation>
+    </message>
+    <message>
+        <source>Duplex:</source>
+        <translation>Duplex:</translation>
+    </message>
+    <message>
+        <source>Draw border</source>
+        <translation>Rahmen anzeigen</translation>
+    </message>
+    <message>
+        <source>Print in reverse order</source>
+        <translation>In umgekehrter Reihenfolge drucken</translation>
+    </message>
+    <message>
+        <source>Printer has duplexer</source>
+        <translation>Duplex-Drucker vorhanden</translation>
+    </message>
+    <message>
+        <source>Manual with reverse (suitable for most printers)</source>
+        <translation>Manuell mit umgekehrter Reihenfolge (für die meisten Drucker geeignet)</translation>
+    </message>
+    <message>
+        <source>Manual without reverse</source>
+        <translation>Manuell ohne umgekehrte Reihenfolge</translation>
+    </message>
+    <message>
+        <source>Preferences of &quot;%1&quot;</source>
+        <translation>Einstellungen von &quot;%1&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>Project</name>
+    <message>
+        <source>Boomaga</source>
+        <comment>Error message title</comment>
+        <translation>Boomaga</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>PDF file &quot;%1&quot; is damaged.</source>
+        <translation>PDF-Datei &quot;%1&quot; ist beschädigt.</translation>
+    </message>
+    <message>
+        <source>PDF file &quot;%1&quot; is encripted.</source>
+        <translation>PDF-Datei &quot;%1&quot; ist verschlüsselt.</translation>
+    </message>
+    <message>
+        <source>I can&apos;t open file &quot;%1&quot;</source>
+        <translation>Datei &quot;%1&quot; konnte nicht geöffnet werden</translation>
+    </message>
+</context>
+<context>
+    <name>TmpPdfFile</name>
+    <message>
+        <source>I can read file &apos;%1&apos;</source>
+        <translation>Datei &apos;%1&apos; konnte nicht gelesen werden</translation>
+    </message>
+    <message>
+        <source>I can&apos;t create temporary file &quot;%1&quot;</source>
+        <translation>Temporäre Datei &quot;%1&quot; konnte nicht erstellt werden</translation>
+    </message>
+    <message>
+        <source>Something is wrong. I can't find boomagamerger program.
+Please reinstall me.</source>
+        <translation>Irgendetwas stimmt hier nicht. Das Programm boomagamerger konnte nicht gefunden werden.
+Bitte neu installieren.</translation>
+    </message>
+</context>
+</TS>
