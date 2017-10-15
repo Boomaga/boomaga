4e004e3e5b93e58669ba79d932f49d4cdb9ff656
sokoloff (Alexander) <sokoloff.a@gmail.com>
2017-10-15 11:04:31 +0300

    Drop Qt4 support

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -26,41 +26,14 @@ macro(setByDefault VAR_NAME VAR_VALUE)
 endmacro()
 
 
-macro(getQtVersion QT_VER)
-    option(USE_QT5 "Force use the Qt5." $ENV{USE_QT5})
-    option(USE_QT4 "Force use the Qt4." $ENV{USE_QT4})
-
-    if((USE_QT4 AND USE_QT5) OR
-       (NOT USE_QT4 AND NOT USE_QT5))
-        find_package(Qt4 QUIET)
-
-        if(QT4_FOUND)
-            set(USE_QT4 ON)
-            set(USE_QT5 OFF)
-            set(${QT_VER} 4)
-
-        else()
-            set(USE_QT4 OFF)
-            set(USE_QT5 ON)
-            set(${QT_VER} 5)
-
-        endif()
-    endif()
-
-    if(USE_QT4)
-        status_message("Using Qt4, for building with Qt5 use -DUSE_QT5=Yes option.")
-    else()
-        status_message("Using Qt5, for building with Qt4 use -DUSE_QT4=Yes option.")
-    endif()
-
-endmacro()
-
 
 macro(setFirstUpper VAR_NAME VAR_VALUE)
   string(SUBSTRING ${VAR_VALUE} 0 1 FIRST_LETTER)
   string(TOUPPER ${FIRST_LETTER} FIRST_LETTER)
   string(REGEX REPLACE "^.(.*)" "${FIRST_LETTER}\\1" ${VAR_NAME} "${VAR_VALUE}")
 endmacro()
+
+
 
 macro(add_tests TESTS_DIR)
   option(BUILD_TESTS "Build tests." $ENV{BUILD_TESTS})
